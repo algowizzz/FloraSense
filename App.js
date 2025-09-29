740909986d4bd995c74e844cea66d120b8535035
algowizzz <spruhaperumalla@gmail.com>
2025-09-29 14:23:08 +0530

    created new folders

--- App.js
+++ App.js
@@ -2,10 +2,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
-import HomeScreen from "./screens/HomeScreen";
+import WelcomeScreen from './frontend/screens/WelcomeScreen';
+import LoginScreen from './frontend/screens/LoginScreen';
+import SignupScreen from './frontend/screens/SignupScreen';
+import HomeScreen from './frontend/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
